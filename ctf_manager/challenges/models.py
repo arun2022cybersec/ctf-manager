8dec738656c8f30cb567171c6b3feb7b5320cba8
Arun Chahar <110096566+arun2022cybersec@users.noreply.github.com>
2024-06-08 07:35:21 +0000

    setup models and authentication

--- ctf_manager/challenges/models.py
+++ ctf_manager/challenges/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Challenge(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    flag = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Submission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    submitted_flag = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.challenge}'
